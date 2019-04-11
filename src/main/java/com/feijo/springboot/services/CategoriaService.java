b9daee48555f136045c32418b171cc1f0d96e1ae
Paulo Ricardo Feijo <paulorikrdo.89@gmail.com>
2019-04-10 22:11:39 -0300

    Ajustes no endpoint /categorias/{id}

--- src/main/java/com/feijo/springboot/services/CategoriaService.java
+++ src/main/java/com/feijo/springboot/services/CategoriaService.java
@@ -5,15 +5,22 @@ import org.springframework.stereotype.Service;
 
 import com.feijo.springboot.domain.Categoria;
 import com.feijo.springboot.repositories.CategoriaRepository;
+import com.feijo.springboot.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
-	
+
 	@Autowired
 	private CategoriaRepository repo;
-	
+
 	public Categoria buscar(Integer id) {
+
 		Categoria cat = repo.findOne(id);
+
+		if (cat == null) {
+			throw new ObjectNotFoundException(
+					"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName());
+		}
 		return cat;
 	}
 }
