8cb20868b9124eb4770918ba3473b33acf6a72a3
Paulo Ricardo Feijo <paulorikrdo.89@gmail.com>
2019-04-14 11:33:28 -0300

    Atualização utilizando somente JsonIgnore

--- src/main/java/com/feijo/springboot/domain/Categoria.java
+++ src/main/java/com/feijo/springboot/domain/Categoria.java
@@ -10,21 +10,18 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToMany;
 
-import com.fasterxml.jackson.annotation.JsonManagedReference;
-
 @Entity
-public class Categoria implements Serializable{
+public class Categoria implements Serializable {
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
-	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
-	
-	@JsonManagedReference
-	@ManyToMany(mappedBy="categorias")
+
+	@ManyToMany(mappedBy = "categorias")
 	private List<Produto> produtos = new ArrayList<>();
-	
+
 	public Categoria(Integer id, String nome) {
 		super();
 		this.id = id;
@@ -51,7 +48,6 @@ public class Categoria implements Serializable{
 		this.nome = nome;
 	}
 
-	
 	public List<Produto> getProdutos() {
 		return produtos;
 	}
@@ -85,6 +81,4 @@ public class Categoria implements Serializable{
 		return true;
 	}
 
-	
-	
 }
