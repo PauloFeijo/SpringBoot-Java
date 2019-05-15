6dbceaa804990e0e3832b9aaaa49e41b3c951436
PauloFeijo <paulorikrdo.89@gmail.com>
2019-05-14 21:35:34 -0300

    Implementando SmtpEmailService com servidor do Google

--- src/main/java/com/feijo/springboot/services/DBService.java
+++ src/main/java/com/feijo/springboot/services/DBService.java
@@ -111,7 +111,7 @@ public class DBService {
 		estadoRepository.save(Arrays.asList(est1, est2));
 		cidadeRepository.save(Arrays.asList(c1, c2, c3));
 
-		Cliente cli1 = new Cliente(null, "Maria Silva", "maria@gmail.com", "6465467261216", TipoCliente.PESSOAFISICA);
+		Cliente cli1 = new Cliente(null, "Maria Silva", "paulorikrdo.89@gmail.com", "6465467261216", TipoCliente.PESSOAFISICA);
 
 		cli1.getTelefones().addAll(Arrays.asList("456554215", "6551654651"));
 
